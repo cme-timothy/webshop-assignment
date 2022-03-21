94a30e43627a38f6e3089ef47d102498cf35d51d
cme-timothy <tittis@pop-os.localdomain>
2022-03-21 13:35:51 +0100

    added a hamburgerMenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { productsInCart } from "../Recoil/products/atom";
+import { useState, useEffect } from "react";
+import hamburgerMenuImg from "../assets/hamburger_menu/hamburger_menu_green.png";
 
 function Header() {
   const [customerCart] = useRecoilState(productsInCart);
+  const [hamburgerMenuToggle, sethamburgerMenuToggle] = useState(false);
 
   const allOrders = [];
   allOrders.push(
@@ -28,41 +31,51 @@ function Header() {
       document.documentElement.scrollTop > 50
     ) {
       document.getElementById("header").style.height = "3.5em";
-      document.getElementById("header").style.backgroundColor = "black";
-      document.getElementById("header").style.color = "#00ff41";
       document.getElementById("h1").style.fontSize = "1rem";
-      document.getElementsByClassName("link")[0].style.color = "#00ff41";
-      document.getElementsByClassName("link")[1].style.color = "#00ff41";
-      document.getElementsByClassName("link")[2].style.color = "#00ff41";
+      document.getElementById("hamburgerMenu").style.width = "30px";
+      document.getElementById("mainMenu").style.top = "56px";
     } else {
       document.getElementById("header").style.height = "5em";
-      document.getElementById("header").style.backgroundColor = "white";
-      document.getElementById("header").style.color = "black";
       document.getElementById("h1").style.fontSize = "2rem";
-      document.getElementsByClassName("link")[0].style.color = "black";
-      document.getElementsByClassName("link")[1].style.color = "black";
-      document.getElementsByClassName("link")[2].style.color = "black";
+      document.getElementById("hamburgerMenu").style.width = "36px";
+      document.getElementById("mainMenu").style.top = "80px";
     }
   }
 
-  console.log(document.getElementsByClassName("link"));
-  console.log(document.getElementById("header"));
+  useEffect(() => {
+    if (hamburgerMenuToggle === true) {
+      document.getElementById("mainMenu").style.display = "flex";
+    } else {
+      document.getElementById("mainMenu").style.display = "none";
+    }
+  });
+
+  function handleClick() {
+    sethamburgerMenuToggle(!hamburgerMenuToggle);
+  }
 
   return (
     <div id="header">
       <h1 id="h1">$Tung-Store</h1>
-      <div className="main-menu">
-        <Link className="link" to="/">
+      <div id="mainMenu">
+        <Link className="link" onClick={handleClick} to="/">
           /Home
         </Link>
-        <Link className="link" to="/produkter">
+        <Link className="link" onClick={handleClick} to="/produkter">
           /Produkter
         </Link>
         <Link
           className="link"
+          onClick={handleClick}
           to="/varukorg"
         >{`/Varukorg(${sumAllOrders})`}</Link>
       </div>
+      <img
+        id="hamburgerMenu"
+        onClick={handleClick}
+        src={hamburgerMenuImg}
+        alt="Menu icon"
+      ></img>
     </div>
   );
 }
