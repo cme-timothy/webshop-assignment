c108f57a14e09746422cbb96e2f98079ff3299de
cme-timothy <tittis@pop-os.localdomain>
2022-03-21 19:50:34 +0100

    homepage styling functional

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,26 @@ function Home() {
         <title>Den bästa affären för allt - Tung Store</title>
       </Helmet>
       <h2>Välkommen till Tung Store</h2>
-      <Link to="/produkter">Produkter</Link>
+      <p className="store-info">
+        Lorem ipsum dolor sit amet consectetur adipiscing elit vel venenatis
+        condimentum auctor pulvinar sed justo, gravida malesuada aliquet ac id
+        sodales semper cursus tortor dui curae senectus mollis. Justo libero
+        bibendum commodo orci nulla neque donec lorem ligula mollis facilisi,
+        venenatis euismod nascetur placerat vulputate vestibulum eget
+        condimentum hendrerit interdum. Tortor magnis posuere aliquet vel
+        laoreet ad, inceptos commodo libero sapien sit ullamcorper, cum aptent
+        fusce cras et. Praesent erat integer viverra cum leo congue rhoncus
+        luctus porta dolor cubilia, laoreet nibh tortor convallis ac facilisis
+        sollicitudin id consectetur lacinia dui, suscipit tempor maecenas mollis
+        suspendisse tincidunt metus tempus sed cras.
+        {<div className="product-info-end">
+        {
+          <Link className="link-to-product-page" to="/produkter">
+            Ta en titt på våra fina produkter
+          </Link>
+        }
+        {<p className="smileyface">☻</p>} </div> }
+      </p>
     </div>
   );
 }
